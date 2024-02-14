22dc95d4d8c295576acd2810b41df146145eebcf
Munteanu Florin <fofo.munteanu@gmail.com>
2024-02-14 14:46:21 +0200

    homework

--- homework/function.js
+++ homework/function.js
@@ -9,14 +9,14 @@ function NumberFromOneTo20(number) {
 
 //Prob 2: returneaza numerele impare pana la 20
 function returneazaNrImpare(number) {
-    let numere_impare = [];
-    for (let i = 1; i <= number; i++) {
-      if (i % 2 != 0) {
-        numere_impare.push(i);
-      }
+  let numere_impare = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 2 != 0) {
+      numere_impare.push(i);
     }
-    console.log('Numelere imparte pana la 20 sunt:' +numere_impare);
   }
+  console.log("Numelere imparte pana la 20 sunt:" + numere_impare);
+}
 
 //Suma elementelor unui array
 function sumArray() {
@@ -48,27 +48,40 @@ function sumElementsArrayGiven() {
     if (typeof userNumber !== "object") {
       verify_number = Number(userNumber);
       nr_max.push(verify_number);
-        } else {
+    } else {
       verify_number = "object";
     }
-
   } while (verify_number !== "object");
 
-console.log('Nr max din array-ul dat este: ' + Math.max(...nr_max));
+  console.log("Nr max din array-ul dat este: " + Math.max(...nr_max));
 }
 
-function reverse(){
-    var item = [0, 1, 0, 1];
-    var item2= [1, 0, 1, 0];
-   
-    for (let i = 0; i < 4; i++) {
-        if(i % 2 == 0){
-            console.log(item);
-        }else {
-            console.log(item2);
-        }
-      }
-   
+function reverse() {
+  var item = [0, 1, 0, 1];
+  var item2 = [1, 0, 1, 0];
+
+  for (let i = 0; i < 4; i++) {
+    if (i % 2 == 0) {
+      console.log(item);
+    } else {
+      console.log(item2);
+    }
+  }
+}
+
+
+// De cate ori se repeta un element
+function elemRepeat(number) {
+  var item = [1, 3, 4, 5, 6, 7, 5];
+  var nr = 0;
+
+  for (let i = 0; i < item.length; i++) {
+    if (number == item[i]) {
+        nr++;
+    }
+  }
+  console.log(`Numarul ${number} se repeta de: ${nr}`);
+
 }
 
 let homework1 = NumberFromOneTo20(20);
@@ -77,4 +90,4 @@ let sum_elements_array = sumArray();
 let max_of_array = maxOfArray();
 let sum_elements_array_dat = sumElementsArrayGiven();
 let repeat = reverse();
-
+let repeat_number = elemRepeat(5);
