f713efa83d94843f699e3e724c7ab8244de148fa
Munteanu Florin <fofo.munteanu@gmail.com>
2024-02-15 12:49:55 +0200

    evenimente

---
+++ eveniment_js/function.js
@@ -0,0 +1,31 @@
+
+let btn = document.getElementById('btn_Apasa');
+let btn2 = document.getElementById('btn_Apasa2');
+
+// Daca nu pui si paranteze rotunde nu apelezi functia ca sa-ti returneze valoarea din ea
+let cnt = 0;
+
+function onApasaClick(e){
+cnt++;
+    e.target.textContent = `Apasa ${cnt}`;
+}
+
+btn.onclick = onApasaClick;
+
+let cnt2 = 0;
+function onApasaClick2(e){
+    cnt2++;
+        e.target.textContent = `Apasa ${cnt2}`;
+    }
+    btn2.addEventListener('click', onApasaClick2);
+
+btn2.addEventListener ('click', function(e){
+alert('am mai pus un event pe addEventListener')
+});
+
+// inregistram functia ca handle
+
+// Ca sa stergem un handler pentru un eveniment putem seta null pe onclick sau folosim functia removeEventListener
+btn2.removeEventListener('click', onApasaClick2);
+
+
