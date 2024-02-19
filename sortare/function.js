989f49ff2ab7b9e542f31c47742efe4ae62a4bba
florin <fofo.munteanu@gmail.com>
2024-02-19 19:18:22 +0200

    sortare

--- sortare/function.js
+++ sortare/function.js
@@ -1,3 +1,50 @@
+const cities = ['Iasi', 'Cluj-Napoca', 'Bucuresti', 'Timisoara', 'Brasov'];
+const sortare_numbers = [10, 4, 5, 77, 8, -2, 0];
+// sortare alfabetica
+let sortare_alfabetica = cities.sort();
+let sortare_numerica = sortare_numbers.sort();
+console.log('Sortare alfabetica a unui array de string:' + sortare_alfabetica);
+console.log('Sortare in ordine crescatoare a unui array de numere: ' + sortare_numerica);
+
+console.log('Scoatem orasele care incep cu litera b');
+// Scoatem orasele care incep cu litera b (filtrare asa se face)
+const literab = cities.filter(function(elem){
+ return elem[0] == 'B';
+});
+
+console.log(literab);
+
+console.log('Sortare orase obiect');
+const advCities = [
+  {
+    name: 'Iasi',
+    rating: 7
+  },
+  {
+    name : 'Cluj-Napoca',
+    rating: 8
+  },
+  {
+    name : 'Bucuresti',
+    rating: 18
+  },
+  {
+    name : 'Timisioara',
+    rating: 10
+  }
+];
+
+// aici are 2 parametri
+// daca a - b...face sa fie sortarea asc
+advCities.sort(function(a, b){
+  //  return b.rating - a.rating;
+  // sortare descrescatoare
+    return a.rating - b.rating;
+});
+console.log(advCities);
+
+
+
 let arr = [
   {
     name: "Maricica",
