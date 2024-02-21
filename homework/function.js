62441a3a3d43d576d289ca1ca81d889e8b3f4326
Munteanu Florin <fofo.munteanu@gmail.com>
2024-02-21 14:02:36 +0200

    commit tema

--- homework/function.js
+++ homework/function.js
@@ -1,92 +1,59 @@
-//Prob 1: returneaza numerele pana la 20
-function numbersFromOneTo20() {
-  let numere = "";
-  for (let i = 1; i <= 20; i++) {
-    numere = numere + i + ";";
+function Suma() {
+  let number1 = document.querySelector("#number1");
+  let nr1 = Number(number1.value);
+  let number2 = document.querySelector("#number2");
+  let nr2 = Number(number2.value);
+  let sum = nr1 + nr2;
+  console.log("Suma este: " + sum);
+};
+
+function Diferenta() {
+  let number1 = document.querySelector("#number1");
+  let nr1 = Number(number1.value);
+  let number2 = document.querySelector("#number2");
+  let nr2 = Number(number2.value);
+
+  let dif = nr1 - nr2;
+  console.log("Rezultatul diferentei este: " + dif);
+};
+
+function Inmultire() {
+  let number1 = document.querySelector("#number1");
+  let nr1 = Number(number1.value);
+  let number2 = document.querySelector("#number2");
+  let nr2 = Number(number2.value);
+
+  let inmultire = nr1 * nr2;
+  console.log("Rezultatul inmultiri este: " + inmultire);
+};
+
+function Impartire() {
+  let number1 = document.querySelector("#number1");
+  let nr1 = Number(number1.value);
+  let number2 = document.querySelector("#number2");
+  let nr2 = Number(number2.value);
+  let impartire = 0;
+
+  if (nr2 != 0) {
+    impartire = nr1 / nr2;
+  } else {
+    impartire = 0;
   }
-  console.log(
-    "numbersFromOneTo20()=> Afiseaza numerele de la 1 to 20: " + numere
-  );
-}
 
-//Prob 2: returneaza numerele impare pana la 20
-function numbersOddOneTo20() {
-  let numere_impare = [];
-  for (let i = 1; i <= 20; i++) {
-    if (i % 2 != 0) {
-      numere_impare.push(i);
-    }
-  }
-  console.log(
-    "numbersOddOneTo20()=> Afiseaza numelere imparte pana la 20: " +
-      numere_impare
-  );
-}
+  console.log("Rezultatul Impartirii este: " + impartire);
+};
 
-// Prob 3: Suma elementelor unui array dat
-function sumElementsArrayGiven() {
-  let item = [1, 2, 4, 5, 6, 0, -1];
-  let sum = 0;
-  for (let i = 0; i < item.length; i++) {
-    sum += item[i];
-  }
-  console.log(
-    "sumElementsArrayGiven()=> Suma elementelor a array-ului: " +
-      "[" +
-      item +
-      "]" +
-      " este: " +
-      sum
-  );
-}
+function calculeaza() {
+  let number1 = document.querySelector("#number1");
+  let nr1 = Number(number1.value);
+  let number2 = document.querySelector("#number2");
+  let nr2 = Number(number2.value);
 
-//Prob 4: Afiseaza elementul maxim din array
-function maxOfArray() {
-  let item = [1, 4, 64, 7, 4, 9, 45, 0, -1];
-  let max = 0;
-  for (let i = 0; i < item.length; i++) {
-    if (max < item[i]) {
-      max = item[i];
-    }
-  }
-  console.log(
-    `maxOfArray()=> Elementul max din array-ul [${item}] este: ${max}`
-  );
-}
+  Suma(nr1, nr2);
+  Diferenta(nr1, nr2);
+  Inmultire(nr1, nr2);
+  Impartire(nr1, nr2);
+};
 
-// Prob 5: De cate ori se repeta un element in array-ul dat ca parametru
-function elemRepeatFromArray(items_array, number) {
-  let nr = 0;
 
-  for (let i = 0; i < items_array.length; i++) {
-    if (number == items_array[i]) {
-      nr++;
-    }
-  }
-  console.log(
-    `elemRepeatFromArray()=> Numarul ${number} se repeta de: ${nr} ori`
-  );
-}
 
-// Prob 6: Challenge
-console.log('Challenge=> Challenge()');
-function Challenge() {
-console.log('Ex 6: Challenge()');
-  let item = [];
-  for (let i = 0; i < 4; i++) {
-    item.push(i % 2);
-  }
-  console.log(item.join(" "));
-
-  for (let i = 1; i < item.length; i++) {
-    txt = item.reverse();
-    console.log(txt.join(" "));
-  }
-}
-
-let numbers_to_20 = numbersFromOneTo20();
-let numbers_odd_to_20 = numbersOddOneTo20();
-let sum_elements_array_dat = sumElementsArrayGiven();
-let max_of_array = maxOfArray();
-let repeat_number = elemRepeatFromArray([2, 3, 6, 3, 8], 3);
-let prob_6 = Challenge();
