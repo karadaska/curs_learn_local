6871fd30b74d8aa486ed522c4311d68b8bae6b51
florin <fofo.munteanu@gmail.com>
2024-03-20 22:58:52 +0200

    fetch

--- fetch/function.js
+++ fetch/function.js
@@ -1,5 +1,12 @@
-async function picioare(){
-    const lista = await fetch ("");
+// await zica nu-mi executa linia 4 daca nu am raspuns de la 3
+const elem_btn = document.querySelector('.btn');
+
+const elem_display = document.querySelector('.display');
+
+async function categorii(){
+    const lista = await fetch ("https://api.chucknorris.io/jokes/categories");
     const list = await lista.json();
-    console.log(list);
+    elem_display.textContent = list;
 }
+
+elem_btn.addEventListener('click', categorii);
