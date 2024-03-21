e8d9455f91cb2f068ca6c494c35f79f0c89f377c
Munteanu Florin <fofo.munteanu@gmail.com>
2024-03-21 16:05:15 +0200

    tete

--- tema fetch/function.js
+++ tema fetch/function.js
@@ -1,8 +1,26 @@
 
-async function animals(){
+const lista_dogs= document.querySelector('.lista_dogs');
+const btn = document.querySelector('#fetch-quote-btn');
+const display_div = document.querySelector('.display-quote');
+
+async function getListaCatei(){
     const lista_animale = await fetch ("https://dog.ceo/api/breeds/list/all");
     const list = await lista_animale.json();
+ 
+ // const option_select = document.createElement('option');
+    // option_select.textContent = list;
+    // lista_dogs.append(option_select);
 
-    console.log(list);
+    display_div.textContent = list.value;
+
 }
 
+
+btn.addEventListener('click', getListaCatei);
+
+
+//     list.forEach(element => {
+//  const option_select = document.createElement('option');
+//     option_select.textContent = element.value;
+//     lista_dogs.append(option_select);
+// });
