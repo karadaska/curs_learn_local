42869121c64f67cdca753f4ab63c57a26bc428cd
florin <fofo.munteanu@gmail.com>
2024-03-25 00:15:04 +0200

    push

--- tema fetch/function.js
+++ tema fetch/function.js
@@ -1,28 +1,26 @@
 const lista_dogs = document.querySelector(".lista_dogs");
 const btn = document.querySelector("#fetch-quote-btn");
 const display_div = document.querySelector(".display-quote");
+const select_option = document.createElement("option");
 
-async function getListaCatei() {
+async function getListDogs() {
   // const lista_animale = await fetch ("https://dog.ceo/api/breeds/list/all");
-  const lista_animale = await fetch(
-    "https://api.chucknorris.io/jokes/categories"
-  );
-  const list = await lista_animale.json();
+  const lista_items = await fetch("https://api.chucknorris.io/jokes/categories");
+  const list = await lista_items.json();
+};
 
 
-      list.forEach(element => {
-  //  const option_select = document.createElement('option');
-  //     option_select.textContent = element.value;
-  //     lista_dogs.append(option_select);
-  console.log(element);
+console.log(getListDogs)
 
-  });
+// list.forEach((element) => {
+//   select_option.textContent = element;
+//   lista_dogs.append(option_select);
+// });
 
-  //  const option_select = document.createElement('option');
-  //     option_select.textContent = list;
-  //     lista_dogs.append(option_select);
+//  const option_select = document.createElement('option');
+//     option_select.textContent = list;
+//     lista_dogs.append(option_select);
 
-  // display_div.textContent = list.value;
-}
+// display_div.textContent = list.value;
 
-btn.addEventListener("click", getListaCatei);
+btn.addEventListener("click", getListDogs);
