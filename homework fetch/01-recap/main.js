abb89ccd7ce1300b4dcb57cba8ef68890935dda7
florin <fofo.munteanu@gmail.com>
2024-03-27 15:45:21 +0200

    fetch

---
+++ homework fetch/01-recap/main.js
@@ -0,0 +1,44 @@
+
+async function getQuote() {
+    displayDiv.textContent = 'Retrieving quote...';
+
+    // select the selected category option
+    const selectedOption = categoriesDropdown.querySelector('option:checked');
+    const categoryName = selectedOption.textContent;
+
+    // make the request
+    const response = await fetch(`https://api.chucknorris.io/jokes/random?category=${categoryName}`);
+
+    // parse the responsa as a JSON
+    const quote = await response.json();
+
+    // display the quote
+    displayDiv.textContent = quote.value;
+}
+
+
+async function getCategories() {
+    const response = await fetch("https://api.chucknorris.io/jokes/categories");
+    // const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    const categories = await response.json();
+    
+    // create the dropdown options
+    categories.forEach(categoryName => {
+        const option = document.createElement('option');
+        option.textContent = categoryName;
+        categoriesDropdown.append(option);
+    });
+
+    // enable the buttton
+    // fetchBtn.disabled = false;
+    fetchBtn.removeAttribute('disabled');
+}
+
+
+getCategories();
+
+const fetchBtn = document.querySelector(`#fetch-quote-btn`);
+fetchBtn.addEventListener(`click`, getQuote);
+
+const displayDiv = document.querySelector(`.display-quote`);
+const categoriesDropdown = document.querySelector(`#categories-dropdown`);
