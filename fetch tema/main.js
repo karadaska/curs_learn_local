da918ee84422f93700eea390e934493f95aca8ab
florin <fofo.munteanu@gmail.com>
2024-03-28 23:33:42 +0200

    img

--- fetch tema/main.js
+++ fetch tema/main.js
@@ -20,20 +20,14 @@ getListaDogs();
 
 async function imagePictureDog() {
   const displayDiv = document.querySelector(`.display`);
-    const selectedOption = categoriesDropdown.querySelector('option:checked');
-    const dog = selectedOption.textContent;
+  const selectedOption = categoriesDropdown.querySelector("option:checked");
+  const dog = selectedOption.textContent;
 
-    const response = await fetch(`https://dog.ceo/api/breed/${dog}/images`);
-    const quote = await response.json();
-    const data = quote.message;
+  const response = await fetch(`https://dog.ceo/api/breed/${dog}/images`);
+  const quote = await response.json();
+  const data = quote.message;
 
-    for (p in data) {
-      const para = document.createElement("p");
-      // displayDiv.textContent = data[p];
-      displayDiv.innerHTML = `<img src="${data[p]}" alt="${p}" width="500" height="600">`;
-
-    }
-
+  for (p in data) {
+    displayDiv.innerHTML = `<img class="display_img" src="${data[p]}" alt="${p}">`;
+  }
 }
-
-
