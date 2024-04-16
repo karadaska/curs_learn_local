c531133e22514fb3796e46e25417fb718b484072
Munteanu Florin <fofo.munteanu@gmail.com>
2024-04-16 14:17:05 +0300

    arrow function

---
+++ arrow_function/function.js
@@ -0,0 +1,16 @@
+function sum (a, b){
+    return a + b;
+}
+
+console.log(sum(1, 2));
+
+let printName = name => {
+    console.log(name);
+}
+
+console.log(printName('teste'));
+
+// Functioneaza asa pentru ca returneaza rezultatul pe un singur rand
+let sumArrow = (a, b) => a + b;
+
+
