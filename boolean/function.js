7e0484ed207c2cc9adef8fa2e67cc1de2daf8855
Munteanu Florin <fofo.munteanu@gmail.com>
2024-04-18 15:58:56 +0300

    boolean

--- boolean/function.js
+++ boolean/function.js
@@ -1,74 +1,18 @@
-let demo = document.getElementById('demo');
-let demo2 = document.getElementById('demo2');
-let demo3 = document.getElementById('demo3');
 
-const a = [1, 2, 3, 4, 5];
+a = true;
+b = false;
+console.log('a == a');
+console.log('valoare: a = ' + ' ' + a + ' valoare a = ' + a + " " + "a este egal cu a : rezultat =>" + (a == a));
 
-a.forEach(elem =>{
-    console.log(elem);
-})
+console.log('b == b');
+console.log('valoare: b = ' + ' ' + b + ' valoare b = ' + b + " " + "rezultat =>" + (b == b));
 
-a.forEach((number, index) => {
-    console.log(number + ' ' + index);
-})
+console.log('b != b');
+console.log('valoare: b = ' + ' ' + b + ' valoare b = ' + b + " " + "rezultat =>" + (b != b));
 
+console.log('!b != !b');
+console.log('valoare: b = ' + ' ' + !b + ' valoare b = ' + !b + " " + "rezultat =>" + (!b != !b));
 
-const fructe = ["Banana", "Orange", "Apple", "Mango"];
-let fLen = fructe.length;
-
-let text = "<ul>";
-for (let i = 0; i < fLen; i++) {
-  text += "<li>" + fructe[i] + "</li>";
-}
-text += "</ul>";
-
-demo.innerHTML = text;
-
-
-const fructe2 = ["Banana", "Orange", "Apple", "Mango"];
-
-let text2 = "<ul>";
-fructe2.forEach(myFunction);
-text2 += "</ul>";
-
-function myFunction(value) {
-  text2 += "<li>" + value + "</li>";
-}
-
-demo2.innerHTML = text2;
-
-
-const numbers = [45, 4, 9, 16, 25];
-let txt3 = "";
-numbers.forEach(myNumere);
-
-function myNumere(value, index, array) {
-    txt3 += value + ' ' +  index +  ' ' + array + "<br>";
-}
-
-demo3.innerHTML = txt3;
-
-// Functia map
-
-const array_map = [1, 2, 3, 4];
-const variabila_map = array_map.map(testMap);
-
-function testMap(number){
-    return number * 2;
-}
-
-console.log(variabila_map);
-
-// array filter
-
-const array_filter = [45, 4, 9, 16, 25];
-console.log(array_filter.filter(myFunctionOver));
-
-function myFunctionOver(value) {
-    return value > 18;
-  } 
-
-
-
-
+console.log('!a != !a');
+console.log('valoare: a = ' + ' ' + !a + ' valoare a = ' + !a + " " + "rezultat =>" + (!a != !a));
 
