16c4d3bb32a931cb5f1f21768d46a76d09b4ef41
Munteanu Florin <fofo.munteanu@gmail.com>
2024-04-19 14:51:54 +0300

    prob 3

--- probleme/function.js
+++ probleme/function.js
@@ -1,3 +1,24 @@
+const problema3 = document.querySelector(`#problema3`);
+const btn3 = document.querySelector(`#btn3`);
+const display_prob3 = document.querySelector(`.display_prob3`);
+
+function howIsTheNumber() {
+  nr = problema3.value;
+  if (nr != "") {
+    if (nr > 0) {
+      display_prob3.textContent = 'Numarul este positiv';
+    } else if (nr < 0) {
+      display_prob3.textContent = 'Numarul este neagtiv';
+    } else if (nr == 0) {
+      display_prob3.textContent = 'Numarul este null';
+    }
+  } else {
+    display_prob3.textContent = "Te rog introdu un numar valid";
+  }
+}
+
+btn3.addEventListener("click", howIsTheNumber);
+
 const problema1 = document.querySelector(`#problema1`);
 const btn1 = document.querySelector(`#btn1`);
 const display_prob = document.querySelector(`.display_prob`);
@@ -18,22 +39,6 @@ const problema2 = document.querySelector(`#problema2`);
 const btn2 = document.querySelector(`#btn2`);
 const display_prob2 = document.querySelector(`.display_prob2`);
 
-// function opusNumber() {
-//   nr = problema2.value;
-//   if (nr != "" && nr != -0) {
-//     if (nr > 0) {
-//       display_prob2.textContent = -nr;
-//     } else if (nr < 0) {
-//       display_prob2.textContent = nr;
-//     } else if (nr = 0) {
-//       console.log('intra aici');
-//       display_prob2.textContent = 0;
-//     } else {
-//       display_prob2.textContent = "Te rog introdu un numar valid";
-//     }
-//   }
-// }
-
 function opusNumber() {
   nr = problema2.value;
   if (nr != "") {
@@ -50,3 +55,5 @@ function opusNumber() {
 }
 
 btn2.addEventListener("click", opusNumber);
+
+
