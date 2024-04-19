dcd1985f4e269fc844ccd6583255c0a2a814084f
Munteanu Florin <fofo.munteanu@gmail.com>
2024-04-19 14:47:59 +0300

    tema

--- probleme/function.js
+++ probleme/function.js
@@ -2,35 +2,51 @@ const problema1 = document.querySelector(`#problema1`);
 const btn1 = document.querySelector(`#btn1`);
 const display_prob = document.querySelector(`.display_prob`);
 
-function nextUpNumber(){
+function nextUpNumber() {
   number = problema1.value;
-  if(number !='' && number !=-0){
+  if (number != "" && number != -0) {
     number++;
     display_prob.textContent = number;
-  }else {
-    display_prob.textContent = 'Te rog introdu un numar valid';
+  } else {
+    display_prob.textContent = "Te rog introdu un numar valid";
   }
 }
 
-btn1.addEventListener('click', nextUpNumber);
-
+btn1.addEventListener("click", nextUpNumber);
 
 const problema2 = document.querySelector(`#problema2`);
 const btn2 = document.querySelector(`#btn2`);
 const display_prob2 = document.querySelector(`.display_prob2`);
 
-function opusNumber(){
+// function opusNumber() {
+//   nr = problema2.value;
+//   if (nr != "" && nr != -0) {
+//     if (nr > 0) {
+//       display_prob2.textContent = -nr;
+//     } else if (nr < 0) {
+//       display_prob2.textContent = nr;
+//     } else if (nr = 0) {
+//       console.log('intra aici');
+//       display_prob2.textContent = 0;
+//     } else {
+//       display_prob2.textContent = "Te rog introdu un numar valid";
+//     }
+//   }
+// }
+
+function opusNumber() {
   nr = problema2.value;
-  if(nr !='' && nr !=-0){
-    display_prob2.textContent = nr;
-    if(nr > 0){
-      nr == -nr
+  if (nr != "") {
+    if (nr > 0) {
+      display_prob2.textContent = -nr;
+    } else if (nr < 0) {
+      display_prob2.textContent = -nr;
+    } else if (nr == 0) {
+      display_prob2.textContent = 0;
     }
-  }else {
-    display_prob2.textContent = 'Te rog introdu un numar valid';
+  } else {
+    display_prob2.textContent = "Te rog introdu un numar valid";
   }
 }
 
-btn2.addEventListener('click', opusNumber);
-
-
+btn2.addEventListener("click", opusNumber);
